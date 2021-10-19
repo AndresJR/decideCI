469e1ae117a518de66f5243f32c5afad473465a6
Andres <ajramirez@us.es>
2021-10-19 16:25:25 +0200

    first commit

---
+++ decide/travis_local_settings.py
@@ -0,0 +1,11 @@
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
